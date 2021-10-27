bef5937549851e87dc149a5b10fb2ba99e765d71
Daniel Khaapamyaki <khaapamyaki@gmail.com>
2021-10-28 00:33:36 +0200

    Remove logger from `Waterdrop`

--- config/initializers/waterdrop.rb
+++ config/initializers/waterdrop.rb
@@ -1,5 +1,3 @@
-Rails.logger = Logger.new(STDOUT)
-
 $producer = WaterDrop::Producer.new
 
 $producer.setup do |config|
@@ -8,8 +6,4 @@ $producer.setup do |config|
     'bootstrap.servers': 'localhost:29092',
     'request.required.acks': 1
   }
-end
-
-$producer.monitor.subscribe('message.produced_async') do |event|
-  Rails.logger.debug "A message: '#{event[:message][:payload]} was produced to topic: '#{event[:message][:topic]}'!"
 end
