6f6e5de0c87cc587ed4d0126383a6fd991537e23
Daniel Khaapamyaki <khaapamyaki@gmail.com>
2021-10-28 20:13:32 +0200

    Add terminal `GameworldPrinter`

--- app/models/planet.rb
+++ app/models/planet.rb
@@ -2,6 +2,12 @@ class Planet < ApplicationRecord
   enum planet_type: %i[default spawn spacestation], _default: :default, _suffix: true
   validates :planet_type, inclusion: { in: planet_types.keys }
 
+  attribute :movement_difficulty, :integer, default: 0
+  attribute :recharge_multiplicator, :integer, default: 0
+
+  validates :movement_difficulty, numericality: { greater_than_or_equal_to: 0 }
+  validates :recharge_multiplicator, numericality: { greater_than_or_equal_to: 0 }
+
   belongs_to :gameworld
 
   has_and_belongs_to_many :neighbours,
---
+++ app/printer/gameworld_printer.rb
@@ -0,0 +1,31 @@
+class GameworldPrinter
+  def self.print_gameworld(gameworld, margin_width = 2)
+    gameworld = gameworld_to_2d_array(gameworld)
+
+    column_widths = []
+    gameworld.each do |row|
+      row.each.with_index do |cell, column_num|
+        column_widths[column_num] = [column_widths[column_num] || 0, cell.to_s.size].max
+      end
+    end
+
+    puts(
+      gameworld.collect do |row|
+        row.collect.with_index do |cell, column_num|
+          cell.to_s.ljust(column_widths[column_num] + margin_width)
+        end.join
+      end
+    )
+  end
+
+  def self.gameworld_to_2d_array(gameworld)
+    grid_size = Math.sqrt(gameworld.planets.size) - 1
+
+    (0..grid_size).map do |x|
+      (0..grid_size).map do |y|
+        planet = gameworld.planets.find { |p| p.x == x && p.y == y }
+        "#{planet.planet_type} #{planet.movement_difficulty} #{planet.recharge_multiplicator}"
+      end
+    end
+  end
+end
