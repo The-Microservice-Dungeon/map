20e68b97ffead3eef8ecb81b1404b8851094a4f0
naskichan <51368274+naskichan@users.noreply.github.com>
2021-10-28 15:53:37 +0200

    Add `SpacestationCreation` implementation (#38)
    * generated space_station code
    
    * testing that a spacestation_creation is not nil
    
    Co-authored-by: Naskichan <gitlab@naski.org>

---
+++ spec/models/spacestation_creation_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.describe SpacestationCreation, type: :model do
+  it 'creates a new spacestation_creation' do
+    spacestation_creation = create(:spacestation_creation)
+    expect(spacestation_creation.id).to be_truthy
+    expect(spacestation_creation.planet_id).to be_truthy
+  end
+end
