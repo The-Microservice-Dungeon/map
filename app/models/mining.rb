2047a0406a7574cb4e859686e19a6cb78e25448d
Daniel Khaapamyaki <khaapamyaki@gmail.com>
2021-10-28 12:52:33 +0200

    Refactor `execute_mining` calculations

--- app/models/mining.rb
+++ app/models/mining.rb
@@ -1,5 +1,5 @@
 class Mining < ApplicationRecord
   belongs_to :planet
   belongs_to :resource_type
-  validates :amount_mined, numericality: { greater_than: 0 }
+  validates :amount_mined, numericality: { greater_than_or_equal_to: 0 }
 end
--- app/models/resource.rb
+++ app/models/resource.rb
@@ -8,14 +8,16 @@ class Resource < ApplicationRecord
   scope :of_type, ->(resource_type_id) { where(resource_type_id: resource_type_id) }
 
   def execute_mining(amount_mined)
+    amount_mined = [amount_mined, 0].max
     diff = current_amount - amount_mined
-    actual_amount_mined = diff >= 0 ? amount_mined : (amount_mined + diff)
+    actual_amount_mined = [amount_mined, (amount_mined + diff)].min
 
     mining = Mining.create!(amount_mined: actual_amount_mined, planet_id: planet_id,
                             resource_type_id: resource_type_id)
-    self.current_amount = diff >= 0 ? diff : 0
 
-    # $producer.publish_async(topic: "resource_mined", payload: mining.to_json)
+    self.current_amount = [diff, 0].max
+
     mining
+    # $producer.publish_async(topic: "resource_mined", payload: mining.to_json)
   end
 end
