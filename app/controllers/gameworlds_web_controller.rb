e8ef12c6a6c3f74508c6146fa509eb5b476c9a37
tsteffe <tristan.steffen@smail.th-koeln.de>
2021-11-06 19:38:43 +0100

    added Icons and styling to Gamweorlds_web show

--- app/controllers/gameworlds_web_controller.rb
+++ app/controllers/gameworlds_web_controller.rb
@@ -1,4 +1,5 @@
 class GameworldsWebController < ApplicationController
+  skip_before_action :verify_authenticity_token
 
   def index
     @gameworlds = Gameworld.all
@@ -7,5 +8,15 @@ class GameworldsWebController < ApplicationController
   def show
     @gameworld = Gameworld.find(params[:id])
     @gameworld2d = GameworldPrinter.gameworld_to_2d_array(@gameworld)
+  end
+
+  def create
+    gameworld_builder = GameworldBuilder.create_regular_gameworld(10,20,1000)
+    @gameworld = gameworld_builder.gameworld
+
+    if @gameworld.save
+      gameworld_builder.finalize_async
+
+    end
   end
 end
