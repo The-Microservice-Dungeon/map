a5b5cc7fc65d8d84e9ac74f0a35beb312cbf7e1b
Daniel Khaapamyaki <daniel@superlist.com>
2021-11-27 21:42:29 +0100

    chore: Fetch env variables for db config

--- config/initializers/waterdrop.rb
+++ config/initializers/waterdrop.rb
@@ -3,7 +3,7 @@ $producer = WaterDrop::Producer.new
 $producer.setup do |config|
   config.deliver = true
   config.kafka = {
-    'bootstrap.servers': 'localhost:29092',
+    'bootstrap.servers': ENV.fetch('KAFKA_BOOTSTRAP_ADDRESS', 'localhost:29092'),
     'request.required.acks': 1
   }
 end
