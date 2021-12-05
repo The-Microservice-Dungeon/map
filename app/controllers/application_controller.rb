9e59fd2eb98e179b1dd1422ce3ca00925ba38c72
Daniel Khaapamyaki <daniel@superlist.com>
2021-12-05 22:26:44 +0100

    refact: Add `WebController` for non api calls

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,4 @@
-class ApplicationController < ActionController::Base
+class ApplicationController < ActionController::API
   after_action :save_last_request_time
 
   def render_not_found(exception)
--- app/controllers/gameworlds_web_controller.rb
+++ app/controllers/gameworlds_web_controller.rb
@@ -1,4 +1,4 @@
-class GameworldsWebController < ApplicationController
+class GameworldsWebController < WebController
   skip_before_action :verify_authenticity_token
 
   def index
