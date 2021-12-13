82cbc01fd42ec94752a854843499266dd8d583e1
Daniel Khaapamyaki <daniel@superlist.com>
2021-12-13 13:13:44 +0100

    test: Unit test `direction_from_neighbour`

--- spec/models/planet_spec.rb
+++ spec/models/planet_spec.rb
@@ -15,4 +15,29 @@ RSpec.describe Planet, type: :model do
     expect(planet.neighbours).to eq([neighbour])
     expect(neighbour.neighbours).to eq([planet])
   end
+
+  describe 'neighbour direction' do
+    let(:gameworld) { create(:gameworld) }
+    let(:planet) { create(:planet, gameworld: gameworld, x: 0, y: 0) }
+
+    it 'returns the neighbours direction as "west"' do
+      neighbour = create(:planet, gameworld: gameworld, x: 1, y: 0)
+      expect(planet.direction_from_neighbour(neighbour)).to eq('west')
+    end
+
+    it 'returns the neighbours direction as "east"' do
+      neighbour = create(:planet, gameworld: gameworld, x: -1, y: 0)
+      expect(planet.direction_from_neighbour(neighbour)).to eq('east')
+    end
+
+    it 'returns the neighbours direction as "north"' do
+      neighbour = create(:planet, gameworld: gameworld, x: 0, y: 1)
+      expect(planet.direction_from_neighbour(neighbour)).to eq('north')
+    end
+
+    it 'returns the neighbours direction as "south"' do
+      neighbour = create(:planet, gameworld: gameworld, x: 0, y: -1)
+      expect(planet.direction_from_neighbour(neighbour)).to eq('south')
+    end
+  end
 end
