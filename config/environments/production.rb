12aa8a82e415cf2ffa3ff3b807282f7e7cabb16f
Daniel Khaapamyaki <dasky@hey.com>
2022-01-03 14:28:15 +0100

    feat: Add delete button to planet view (#98)

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -113,5 +113,5 @@ Rails.application.configure do
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
 
-  config.hosts << 'cides03.gm.fh-koeln.de'
+  config.hosts.clear
 end
